93fb05ff0b4252ce15b557ff7ac7de07d47b33e0
adembogatekin <pektekin99@gmail.com>
2023-04-05 23:43:56 +0300

    05.04.2023

---
+++ 35_storage_apı/myScript.js
@@ -0,0 +1,29 @@
+// localStorage.setItem("name","Jhon Doe")
+// const item = localStorage.getItem("name")
+// console.log(item)
+// localStorage.setItem("name",["Ray Doe","Lee Doe"])
+// const item2 = localStorage.getItem("name")
+// console.log(item2)
+
+// const name = ["Jhon Doe","Lee Doe"]
+// localStorage.setItem("name",JSON.stringify(name))
+// const item = JSON.parse(localStorage.getItem("name"))
+// console.log(item)
+
+//soru : üzerine yazmayıp ekleme yapması için ne yapabiliriz ?
+// biz sadece name adlı bir veri olduğunu biliyoruz.
+// veri içeriğin bilmiyoruz.
+
+// const name1 =["Jhon Doe","Lee Doe"]
+// localStorage.setItem("name",JSON.stringify(name1));
+// const item = JSON.parse(localStorage.getItem("name"))
+// console.log(item)
+// const name2 = [...JSON.parse(localStorage.getItem("name")),"Jhon Ray","Lee Ray"]
+// localStorage.setItem("name",JSON.stringify(name2));
+// const item2 = JSON.parse(localStorage.getItem("name"));
+// console.log(item2)
+
+// localStorage.removeItem("name");
+// localStorage.clear();
+
+// sessionStorage.setItem("name","Ahmet Berk")
